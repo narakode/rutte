bb10404af87494e995fe7c5b0518292a7e2e3dfb
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-28 15:12:04 +0700

    test: insert

--- src/router.test.ts
+++ src/router.test.ts
@@ -1,19 +1,307 @@
-import { describe, expect, test } from 'vitest';
+import { expect, test } from 'vitest';
 import { Router } from './router.js';
 
-describe('Router', () => {
-  describe('add item', () => {
-    test('should have a root node', () => {
-      const router = new Router();
+test('base', () => {
+  const router = new Router();
 
-      expect(router).toHaveProperty('root');
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [],
+  });
+});
 
-      const root = router.root;
+test('insert 1', () => {
+  const router = new Router();
 
-      expect(root).toEqual({
-        path: '',
+  router.insert('user');
+  router.insert('test');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'user',
         handler: null,
-      });
-    });
+        children: [],
+      },
+      {
+        path: 'test',
+        handler: null,
+        children: [],
+      },
+    ],
+  });
+});
+
+test('insert 2', () => {
+  const router = new Router();
+
+  router.insert('user');
+  router.insert('user/profile');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'user',
+        handler: null,
+        children: [
+          {
+            path: '/profile',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+test('insert 3', () => {
+  const router = new Router();
+
+  router.insert('user');
+  router.insert('userprofile');
+  router.insert('userdashboard');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'user',
+        handler: null,
+        children: [
+          {
+            path: 'profile',
+            handler: null,
+            children: [],
+          },
+          {
+            path: 'dashboard',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+test('insert 4', () => {
+  const router = new Router();
+
+  router.insert('user');
+  router.insert('userprofile');
+  router.insert('userdashboard');
+  router.insert('userproject');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'user',
+        handler: null,
+        children: [
+          {
+            path: 'pro',
+            handler: null,
+            children: [
+              {
+                path: 'file',
+                handler: null,
+                children: [],
+              },
+              {
+                path: 'ject',
+                handler: null,
+                children: [],
+              },
+            ],
+          },
+          {
+            path: 'dashboard',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+test('insert 5', () => {
+  const router = new Router();
+
+  router.insert('userprofile');
+  router.insert('user');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'user',
+        handler: null,
+        children: [
+          {
+            path: 'profile',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+test('insert 6', () => {
+  const router = new Router();
+
+  router.insert('userprofile');
+  router.insert('user');
+  router.insert('userhome');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'user',
+        handler: null,
+        children: [
+          {
+            path: 'profile',
+            handler: null,
+            children: [],
+          },
+          {
+            path: 'home',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+test('insert 7', () => {
+  const router = new Router();
+
+  router.insert('userprofile');
+  router.insert('user');
+  router.insert('usecase');
+  router.insert('userhome');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'use',
+        handler: null,
+        children: [
+          {
+            path: 'r',
+            handler: null,
+            children: [
+              {
+                path: 'profile',
+                handler: null,
+                children: [],
+              },
+              {
+                path: 'home',
+                handler: null,
+                children: [],
+              },
+            ],
+          },
+          {
+            path: 'case',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
+  });
+});
+
+test('insert 8', () => {
+  const router = new Router();
+
+  router.insert('home');
+  router.insert('dashboard');
+  router.insert('user');
+  router.insert('userprofile');
+  router.insert('userproject');
+  router.insert('userproduct');
+  router.insert('userhome');
+  router.insert('usecase');
+
+  expect(router.node).toEqual({
+    path: '',
+    handler: null,
+    children: [
+      {
+        path: 'home',
+        handler: null,
+        children: [],
+      },
+      {
+        path: 'dashboard',
+        handler: null,
+        children: [],
+      },
+      {
+        path: 'use',
+        handler: null,
+        children: [
+          {
+            path: 'r',
+            handler: null,
+            children: [
+              {
+                path: 'pro',
+                handler: null,
+                children: [
+                  {
+                    path: 'file',
+                    handler: null,
+                    children: [],
+                  },
+                  {
+                    path: 'ject',
+                    handler: null,
+                    children: [],
+                  },
+                  {
+                    path: 'duct',
+                    handler: null,
+                    children: [],
+                  },
+                ],
+              },
+              {
+                path: 'home',
+                handler: null,
+                children: [],
+              },
+            ],
+          },
+          {
+            path: 'case',
+            handler: null,
+            children: [],
+          },
+        ],
+      },
+    ],
   });
 });
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,118 @@
-export interface RouterPath<T> {
+interface RoutePath<T> {
   path: string;
   handler: T | null;
-  children?: RouterPath<T>[];
+  children: RoutePath<T>[];
 }
 
 export class Router<T> {
-  root: RouterPath<T> = {
+  node: RoutePath<T> = {
     path: '',
     handler: null,
+    children: [],
   };
+
+  insert(path: string) {
+    if (!this.node.children.length) {
+      this.node.children.push({
+        path,
+        handler: null,
+        children: [],
+      });
+
+      return;
+    }
+
+    let root = this.node;
+    let inserted = false;
+    let search = path;
+
+    while (!inserted) {
+      let longestCommonPrefixStart: number | null = null;
+      let longestCommonPrefixChildIndex: number | null = null;
+
+      let index = 0;
+
+      while (
+        index < root.children.length &&
+        longestCommonPrefixChildIndex === null
+      ) {
+        const child = root.children[index];
+
+        longestCommonPrefixStart = this.countLongestCommonPrefix(
+          child as RoutePath<T>,
+          search,
+        );
+
+        if (longestCommonPrefixStart > 0) {
+          longestCommonPrefixChildIndex = index;
+
+          break;
+        }
+
+        index++;
+      }
+
+      if (longestCommonPrefixChildIndex === null) {
+        root.children.push({
+          path: search,
+          handler: null,
+          children: [],
+        });
+
+        inserted = true;
+
+        break;
+      }
+
+      const parent = root.children[
+        longestCommonPrefixChildIndex
+      ] as RoutePath<T>;
+
+      if (parent.path.length === (longestCommonPrefixStart as number)) {
+        root = parent;
+        search = search.slice(longestCommonPrefixStart as number);
+
+        continue;
+      }
+
+      if (search.length >= (longestCommonPrefixStart as number)) {
+        root = parent;
+
+        const children = parent.children;
+
+        parent.children = [];
+
+        parent.children.push({
+          path: parent.path.slice(longestCommonPrefixStart as number),
+          handler: null,
+          children: children,
+        });
+        parent.path = parent.path.slice(0, longestCommonPrefixStart as number);
+
+        if (parent.path === search) {
+          inserted = true;
+          break;
+        }
+
+        search = search.slice(longestCommonPrefixStart as number);
+
+        continue;
+      }
+    }
+  }
+
+  private countLongestCommonPrefix(child: RoutePath<T>, path: string): number {
+    let commonIndex = 0;
+
+    const maxLength = Math.min(child.path.length, path.length);
+
+    while (
+      commonIndex < maxLength &&
+      child.path[commonIndex] === path[commonIndex]
+    ) {
+      commonIndex++;
+    }
+
+    return commonIndex;
+  }
 }
